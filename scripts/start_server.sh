3062bc73dda773c2d70ffefd8c13235c17da36da
Mujamil Ahamed <tajswas@gmail.com>
2018-05-25 20:23:51 +0530

    added

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,6 @@
 service docker start
 export MOODLE_DOCKER_WWWROOT=/moodle-wwdata
 export MOODLE_DOCKER_DB=pgsql
-cp config.docker-template.php $MOODLE_DOCKER_WWWROOT/config.php
+cp /moodle/moodle-docker/config.docker-template.php $MOODLE_DOCKER_WWWROOT/config.php
 /bin/sh /moodle/moodle-docker/bin/moodle-docker-compose up
 /bin/sh /moodle/moodle-docker/bin/moodle-docker-wait-for-db
