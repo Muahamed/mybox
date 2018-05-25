c5b24904ae70d976f00b04d5579d9a7430eb242c
Mujamil Ahamed <tajswas@gmail.com>
2018-05-25 20:20:54 +0530

    appsec

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-service httpd start
-service mysqld start
+service docker start
+export MOODLE_DOCKER_WWWROOT=/moodle-wwdata
+export MOODLE_DOCKER_DB=pgsql
+cp config.docker-template.php $MOODLE_DOCKER_WWWROOT/config.php
+/bin/sh /moodle/moodle-docker/bin/moodle-docker-compose up
+/bin/sh /moodle/moodle-docker/bin/moodle-docker-wait-for-db
