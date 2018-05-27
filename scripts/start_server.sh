2becd5c43df722529428699b84019de0c83a9574
Mujamil Ahamed <tajswas@gmail.com>
2018-05-28 00:24:43 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,7 @@
 service docker start
 export MOODLE_DOCKER_WWWROOT=/moodle-wwdata
 export MOODLE_DOCKER_DB=pgsql
+/bin/sh /moodle/moodle-docker/bin/moodle-docker-compose down
 cp /moodle/moodle-docker/config.docker-template.php $MOODLE_DOCKER_WWWROOT/config.php
-/bin/sh /moodle/moodle-docker/bin/moodle-docker-compose up
+/bin/sh /moodle/moodle-docker/bin/moodle-docker-compose up -d
 /bin/sh /moodle/moodle-docker/bin/moodle-docker-wait-for-db
