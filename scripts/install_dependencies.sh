7dcec7a6f587e86241ea6e2b0aa256eed1f6c336
Mujamil Ahamed <tajswas@gmail.com>
2018-05-31 18:10:44 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+#!/bin/bash
+export MOODLE_DOCKER_WWWROOT=/moodle-wwdata
+export MOODLE_DOCKER_DB=pgsql
+/bin/sh /moodle/moodle-docker/bin/moodle-docker-compose down
 yum install -y telnet
 rm -rf /moodle-wwdata/*
