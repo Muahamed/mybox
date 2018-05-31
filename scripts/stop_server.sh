a02960bbba5b7f1d5a93a6c2bf44b503391f952b
Mujamil Ahamed <tajswas@gmail.com>
2018-05-31 17:09:05 +0530

    Update stop_server.sh
    Test code

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-isExistApp=`pgrep httpd`
-if [[ -n  $isExistApp ]]; then
-   echo "hello exit"
-fi
-isExistApp=`pgrep mysqld`
-if [[ -n  $isExistApp ]]; then
-   echo "hello exit"
-fi
+echo " Test to run"
+#isExistApp=`pgrep httpd`
+#if [[ -n  $isExistApp ]]; then
+#   echo "hello exit"
+#fi
+#isExistApp=`pgrep mysqld`
+#if [[ -n  $isExistApp ]]; then
+#   echo "hello exit"
+#fi
