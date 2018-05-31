a9390e86c42137ab529bdd74c723f011199a6406
Mujamil Ahamed <tajswas@gmail.com>
2018-05-31 18:06:53 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export MOODLE_DOCKER_WWWROOT=/moodle-wwdata
 export MOODLE_DOCKER_DB=pgsql
-/bin/sh /moodle/moodle-docker/bin/moodle-docker-compose down
+
 #isExistApp=`pgrep httpd`
 #if [[ -n  $isExistApp ]]; then
 #   echo "hello exit"
