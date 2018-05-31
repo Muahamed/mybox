101edd65f2baeb0b8c7c9f5c2e05f6f7969c0e98
Mujamil Ahamed <tajswas@gmail.com>
2018-05-31 18:13:15 +0530

    Update start_server.sh
    enw

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export MOODLE_DOCKER_WWWROOT=/moodle-wwdata
 export MOODLE_DOCKER_DB=pgsql
+/bin/sh /moodle/moodle-docker/bin/moodle-docker-compose down
 cp /moodle/moodle-docker/config.docker-template.php $MOODLE_DOCKER_WWWROOT/config.php
 /bin/sh /moodle/moodle-docker/bin/moodle-docker-compose up -d
 /bin/sh /moodle/moodle-docker/bin/moodle-docker-wait-for-db
