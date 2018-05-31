4b43dd21843ea879f23e74a5f50f074b50f45ce8
Mujamil Ahamed <tajswas@gmail.com>
2018-05-31 23:54:59 +0530

    composer added
    docmp

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export MOODLE_DOCKER_WWWROOT=/moodle-wwdata
 export MOODLE_DOCKER_DB=pgsql
-/bin/sh /moodle/moodle-docker/bin/moodle-docker-compose down
+/usr/bin/compose down
 cp /moodle/moodle-docker/config.docker-template.php $MOODLE_DOCKER_WWWROOT/config.php
-/bin/sh /moodle/moodle-docker/bin/moodle-docker-compose up -d
-/bin/sh /moodle/moodle-docker/bin/moodle-docker-wait-for-db
+/usr/bin/compose up -d
